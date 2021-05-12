24810abc121f3403f2578b2324f9f7aab146f02d
Chaiweon <raphee0704@gmail.com>
2021-05-12 15:27:42 +0900

    수정

--- May12th.java
+++ May12th.java
@@ -3,22 +3,45 @@ import java.util.Scanner;
 public class May12th {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		int y = sc.nextInt();
+		int h = sc.nextInt();
+		int m = sc.nextInt();
 		
-		if (x>0) {
-			if(y>0) {
-				System.out.println(1);
-			}else if (y<0) {
-				System.out.println(4);
+//		if (x>0) {
+//			if(y>0) {
+//				System.out.println(1);
+//			}else if (y<0) {
+//				System.out.println(4);
+//			}
+//		}
+//		else if(x<0) {
+//			if(y>0) {
+//				System.out.println(2);
+//			}else if (y<0) {
+//				System.out.println(3);
+//			}
+//		}
+		
+		if(0==h){
+			if(m>=45) {
+				System.out.println(h+ " "+(m-45));
+			}else {
+				int dif=45 - m;
+				//System.out.println(dif);
+				System.out.println( 23 + " " + (60-dif));
 			}
 		}
-		else if(x<0) {
-			if(y>0) {
-				System.out.println(2);
-			}else if (y<0) {
-				System.out.println(3);
+		else  {
+			if(m>=45) {
+				System.out.println(h+ " "+(m-45));
+			}else {
+				int dif=45 - m;
+				//System.out.println(dif);
+				System.out.println( (h-1) + " " + (60-dif));
 			}
+			
 		}
+		
+	
+		
 }
 }
